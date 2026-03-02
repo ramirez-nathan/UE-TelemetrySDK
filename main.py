a3290e2bcb81e34f6f99946aeb6799366d6b0a93
Nathan Ramirez <nater0826@gmail.com>
2026-03-01 22:32:11 -0800

    add item and event schemas

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 app = FastAPI()
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
+class EventIn(BaseModel):
+    name: str
+    timestamp: datetime
+
+class EventOut(EventIn):
+    id: int
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
